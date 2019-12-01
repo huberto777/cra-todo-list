7588470fdd978c35907a9219b943600d1747c4cb
Hubert <huberto777@op.pl>
2019-12-01 10:00:36 +0100

    Updates

---
+++ precache-manifest.c8d219ecb150880123fc5fa4a0e6eaf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b840bcc282b1f8526bbbe9c79ab1658",
+    "url": "/cra-todo-list/index.html"
+  },
+  {
+    "revision": "79dddeec807bf067c538",
+    "url": "/cra-todo-list/static/js/2.8dc9f97a.chunk.js"
+  },
+  {
+    "revision": "04833308f65c7a000d1c",
+    "url": "/cra-todo-list/static/js/main.01e3e631.chunk.js"
+  },
+  {
+    "revision": "20c1fdf4688fefdd6d3a",
+    "url": "/cra-todo-list/static/js/runtime-main.6f1f25b0.js"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/cra-todo-list/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/cra-todo-list/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "5866c2c688e22110ac70e41fcc536b70",
+    "url": "/cra-todo-list/static/media/rubbish-bin.5866c2c6.svg"
+  }
+]);
